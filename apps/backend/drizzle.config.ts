25a9b1c74cacaf88cc2f43d4ca264eee60e0c7fb
Brian <brian.w.tjahjadi@gmail.com>
2025-04-05 19:37:17 +1100

    drizzle scaffold 2

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import { Config, defineConfig } from 'drizzle-kit';
 
-dotenv.config({ path: '../../.env' }); // don't like this
+dotenv.config({ path: '../../.env'}); // don't like this
 
 console.log("DATABASE_URL:" + process.env.DATABASE_URL);
 
