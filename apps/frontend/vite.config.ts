f7271fea8bf0f2ad24ded8ab9009e1c561c96259
Brian <brian.w.tjahjadi@gmail.com>
2025-04-07 22:15:00 +1000

    added shadcn

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,8 +1,15 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import relay from "vite-plugin-relay";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [relay, react()],
+  plugins: [relay, react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
